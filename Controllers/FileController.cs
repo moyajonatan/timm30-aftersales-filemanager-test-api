08af0854a7c13f5acd730dd96205b3747bab06a0
Sebastian Peña Diaz <spena@evoltis.com>
2023-05-19 19:45:29 -0300

    Complete and test GmPriceList Options

--- Controllers/FileController.cs
+++ Controllers/FileController.cs
@@ -33,6 +33,7 @@ namespace Test.Controllers
                 .SetFileTypeEnum(fileTypeEnum)
                 .SetFile(stream, fileName)
                 .Build();
+
             var isValid = fileProcessor.Validate();
             return Ok(isValid);
         }
@@ -59,13 +60,13 @@ namespace Test.Controllers
                 new IndexedColumnOption
                 {
                     DataType = "string", //Descripción del Articulo
-                    Format = "[A-Za-z0-9\\s]",
+                    Format = @"^[A-Za-z0-9\s]*$",
                     Index = 1,
                 },
                 new IndexedColumnOption
                 {
                     DataType = "string", //Modelo
-                    Format = "[A-Za-z0-9\\s]",
+                    Format = @"^[A-Za-z0-9\s]*$",
                     Index = 3,
                 },
                 new IndexedColumnOption
@@ -130,14 +131,14 @@ namespace Test.Controllers
                 new TextFieldColumnOption
                 {
                     DataType = "string", //Articulo
-                    Format = @"^[A-Za-z0-9\s]*$",
+                    Format = @"^[A-Za-z0-9]+$",
                     IndexStart = 0,
                     Length = 16
                 },
                 new TextFieldColumnOption
                 {
                     DataType = "string", //Descripcion
-                    Format =  @"^[A-Za-z0-9\s]*$",
+                    Format =  @"^.+$",
                     IndexStart = 17,
                     Length = 30,
                 },
@@ -147,22 +148,27 @@ namespace Test.Controllers
                     IndexStart = 48,
                     Length = 4
                 },
-                // new IndexedColumnOption
-                //{
-                //    DataType = "string", //Compatibilidad
-                //    Index = 5,
-                //},
-                //new IndexedColumnOption
-                //{
-                //    DataType = "string", //Nuevo Código
-                //    Format = "^([A-Z0-9]{12}|-)$",
-                //    Index = 6,
-                //},
-                //new IndexedColumnOption
-                //{
-                //    DataType = "decimal", //Lista de Precio
-                //    Index = 7,
-                //},
+                 new TextFieldColumnOption
+                {
+                    DataType = "string", //Numero pieza reemplazo
+                    Format = @"^\w*$",
+                    IndexStart = 53,
+                    Length = 16
+                },
+                new TextFieldColumnOption
+                {
+                    DataType = "string", //Porcentaje Descuento
+                    Format = @"^\d{2},\d{2}$",
+                    IndexStart = 70,
+                    Length = 5
+                },
+                new TextFieldColumnOption
+                {
+                    DataType = "string", //Precio Venta
+                    Format = @"^\d+,\d{2}$",
+                    IndexStart = 76,
+                    Length = 10
+                },
                 //new IndexedColumnOption
                 //{
                 //    DataType = "decimal",
@@ -217,7 +223,7 @@ namespace Test.Controllers
                  new IndexedColumnOption
                 {
                     DataType = "string", //Linea
-                    Format = "[A-Za-z\\s]",
+                    Format = @"^[A-Za-z\s]$",
                     Index = 2,
                     Page = 3
                 },
